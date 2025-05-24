61debe518b38d2fb6e86e71e703c10b48f6c8387
sumit <debnathsumit558@gmail.com>
2025-05-24 13:07:08 +0530

    removing dark mode

--- app/page.js
+++ app/page.js
@@ -10,23 +10,21 @@ import Work from "./components/Work";
 
 export default function Home() {
   const [isDarkMode, setIsDarkMode] = useState(false);
-
   useEffect(()=>{
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      setIsDarkMode(true);
-    }else{
+    // if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    //   setIsDarkMode(true);
+    // }else{
       setIsDarkMode(false);
-
-    }
+    // }
   }, []);
   useEffect(()=>{
-    if (isDarkMode) {
-      document.documentElement.classList.add('dark');
-      localStorage.theme = 'dark';
-    }else{
+    // if (isDarkMode) {
+    //   document.documentElement.classList.add('dark');
+    //   localStorage.theme = 'dark';
+    // }else{
       document.documentElement.classList.remove('dark');
       localStorage.theme = '';
-    }
+    // }
   }, [isDarkMode])
   return (
     <>
